46a1a1dce3c1e5fc8e14cb185e7e6bd2003ca0f9
georgecriezis <george.criezis@student.fairfield.edu>
2026-02-08 15:33:24 -0500

    Problem 3

--- Problem 3 Loan Risk Classification.py
+++ Problem 3 Loan Risk Classification.py
@@ -2,6 +2,7 @@
 credit_score = int(input("What is your credit score? "))
 annual_income = float(input("What is your annual income? "))
 
+# conditional statements determine what category of risk you fall into
 if credit_score >= 720 and annual_income >= 60000:
     category = "Low Risk"
 
@@ -10,4 +11,5 @@ elif credit_score >= 650 and annual_income >= 40000:
 else:
     category = "High Risk"
 
+# Use squiggly brackets to print value of category not just 'category'
 print(f'Loan Risk Category: {category}')
